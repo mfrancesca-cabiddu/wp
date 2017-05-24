e6282268120d4f6366eabfeb24247a30acb6c532
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 10:05:42 +0200

    Update index.php

--- wp-content/themes/Francesca/index.php
+++ wp-content/themes/Francesca/index.php
@@ -1,1 +1,77 @@
 <?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"></a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Chi siamo</a></li>
+                            <li><a href="#">Gallerie</a></li>
+                            <li><a href="#">Forum</a></li>
+                            <li><a href="#">Recensioni</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+            <div class="row">
+                <div class="col-md-12 welcome">
+                    <h1>Fotografia di scena:</h1>
+                    <h4>Iscriviti e ricevi gratuitamente la guida via mail!</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- form begin -->
+    <div class="row" id="contact">
+        <div class="col-lg-12 text-center">
+            <h2>Iscriviti alla nostra newsletter</h2>
+            <h5>Sarai aggiornato sulle nostre novità e sul lancio di tutte le nuove offerte.</h5>
+        </div>
+        <form class="effezero-form">
+                <div class="col-lg-12">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                           <label for="exampleInputEmail1">Contattaci</label>
+                           <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Inserisci qui la tua e-mail" required autofocus>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Inserisci qui la tua password" required>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label for="exampleInputEmail1">Richiedi informazioni </label>
+                        <textarea class="form-control" rows="3" required></textarea>
+                    </div>
+                </div>
+                <div class="row">
+            <div class="col-lg-12 text-center">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+            </div>
+        </div>
+    </form> 
+</div>
+<!-- form end --> 
+<script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+<script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/fixtures.js"></script>
+</body>
+</html>
