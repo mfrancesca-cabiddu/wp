999b5def161eae30e5752a761d2faef9125697c8
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 11:19:17 +0200

    Update index.php

--- wp-content/themes/Francesca/index.php
+++ wp-content/themes/Francesca/index.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
-<div class="row">
-    <div class="col-md-12 welcome">
-        <h1><?php bloginfo('name') ?></h1>
-        <h4><?php bloginfo('description') ?></h4>
+            <div class="row">
+                <div class="col-md-12 welcome">
+                    <h1>Fotografia di scena:</h1>
+                    <h4>Iscriviti e ricevi gratuitamente la guida via mail!</h4>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-</div>
-</div>
-<div class="row" id="contact">
-    <div class="col-lg-12 text-center">
-        <h1>Contact us</h1>
-        <h3>Lorem ipsum dolor sit amet consectetur.</h3>
-    </div>
-</div>
-<form>
+    <!-- form begin -->
+    <div class="container-fluid">
+        <div class="row text-center">
+            <h2>Iscriviti alla nostra newsletter</h2>
+            <h5>Sarai aggiornato sulle nostre novità e sul lancio di tutte le nuove offerte.</h5>
+        </div>
     <?php echo do_shortcode('[contact-form-7 id="20" title="Contact form 1"]'); ?>
 </form>
 </div>
