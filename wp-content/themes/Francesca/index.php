037aa44af18cd055fa18dd97b2fa6395b638496e
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 12:37:29 +0200

    Update index.php

--- wp-content/themes/Francesca/index.php
+++ wp-content/themes/Francesca/index.php
@@ -10,8 +10,8 @@
     <!-- form begin -->
     <div class="container-fluid">
         <div class="row text-center">
-            <h2><?php bloginfo(name) ?<>/h2>
-            <h5><?php bloginfo(description) ?<>/h5>
+            <h2><?php bloginfo(name) ?></h2>
+            <h5><?php bloginfo(description) ?></h5>
         </div>
     <?php echo do_shortcode('[contact-form-7 id="20" title="Contact form 1"]'); ?>
 </form>
