4d229cac7862c6accd129901b4c0d2be04b37597
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 11:35:10 +0200

    Update index.php

--- wp-content/themes/Francesca/index.php
+++ wp-content/themes/Francesca/index.php
@@ -10,8 +10,8 @@
     <!-- form begin -->
     <div class="container-fluid">
         <div class="row text-center">
-            <h2>Iscriviti alla nostra newsletter</h2>
-            <h5>Sarai aggiornato sulle nostre novità e sul lancio di tutte le nuove offerte.</h5>
+            <h2><?php bloginfo(name) ?<>/h2>
+            <h5><?php bloginfo(description) ?<>/h5>
         </div>
     <?php echo do_shortcode('[contact-form-7 id="20" title="Contact form 1"]'); ?>
 </form>
