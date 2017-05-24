3a4da4ccf806e70fb59de794b1893d0a2d958b27
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 10:55:28 +0200

    Update functions.php

--- wp-content/themes/Francesca/functions.php
+++ wp-content/themes/Francesca/functions.php
@@ -1,11 +1,12 @@
-<?php 
-function enqueue_css() ?> {
-	wp_enqueue_style ('bs',get_template_directory_uri().'Vendors/css/bootstrap.min.css' );
-	wp_enqueue_style ('bs',get_template_directory_uri().'style.css' );
+<?php
+function enqueue_css() {
+    wp_enqueue_style( 'bs', get_template_directory_uri().'/vendors/css/bootstrap.min.css' );
+    wp_enqueue_style( 'st', get_template_directory_uri().'/style.css' );
 }
-add.action('wp_enqueue_scripts', enqueue.css');
-function enqueue_js () {
-	wp_enqueue_scripts ('bv',get_template_directory_uri) ().'Vendors/js/bootsstrapValidator.min.js', array ('jquery'), '', true);
-	wp_enqueue_scripts ('bv',get_template_directory_uri) ().'/js/main.js', array ('jquery'), '', true);
+add_action( 'wp_enqueue_scripts', 'enqueue_css' );
+function enqueue_js() {
+    wp_enqueue_script( 'bv', get_template_directory_uri().'/vendors/js/bootstrapValidator.min.js', array( 'jquery' ), '', true );
+    wp_enqueue_script( 'mn', get_template_directory_uri().'/js/main.js', array( 'jquery' ), '', true );
 }
-add.action('wp_enqueue_scripts', enqueue.js');
+add_action( 'wp_enqueue_scripts', 'enqueue_js' );
+?>
