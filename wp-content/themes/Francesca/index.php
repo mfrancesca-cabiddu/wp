f0672a0ca9cd2ef4d90f7a1a46dc9a67b151bc55
mfrancesca-cabiddu <m.francesca.cabiddu@gmail.com>
2017-05-24 13:20:33 +0200

    Update index.php

--- wp-content/themes/Francesca/index.php
+++ wp-content/themes/Francesca/index.php
@@ -10,10 +10,35 @@
     <!-- form begin -->
     <div class="container-fluid">
         <div class="row text-center">
-            <h2><?php bloginfo(name) ?></h2>
-            <h5><?php bloginfo(description) ?></h5>
+            <h2><?php bloginfo(name) ?<>/h2>
+            <h5><?php bloginfo(description) ?<>/h5>
         </div>
-    <?php echo do_shortcode('[contact-form-7 id="20" title="Contact form 1"]'); ?>
+ <div class="container-fluid">
+        <div class="row text-center">
+            <h2>Iscriviti alla nostra newsletter</h2>
+            <h5>Sarai aggiornato sulle nostre novità e sul lancio di tutte le nuove offerte.</h5>
+        </div>
+        <form class="effezero-form">
+            <div class="row">
+                <div class="col-md-6">
+                 <div class="form-group">
+                        <label for="exampleInputEmail1">Contattaci</label>
+                           <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Inserisci qui la tua e-mail" required="" autofocus="">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Inserisci qui la tua password" required="">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label for="exampleInputEmail1">Richiedi informazioni </label>
+                        <textarea class="form-control" rows="3" required=""></textarea>
+                    </div>
+                </div>
+                <div class="row text-center">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+            </div>
+        </form></div>
 </form>
 </div>
 </div>
